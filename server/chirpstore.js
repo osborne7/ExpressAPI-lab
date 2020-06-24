f9759c337608a12f7bc6dabc573e0ad3a78b7f9c
osborne7 <eleanor.c.osborne@gmail.com>
2020-06-24 18:33:57 -0400

    Finished lab, advanced section available for further expansion

--- server/chirpstore.js
+++ server/chirpstore.js
@@ -37,5 +37,6 @@ module.exports = {
     DeleteChirp: deleteChirp,
     GetChirps: getChirps,
     GetChirp: getChirp,
-    UpdateChirp: updateChirp
+    UpdateChirp: updateChirp,
+    WriteChirp: writeChirps
 }
